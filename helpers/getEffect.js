2b77ccfde61bda02a080ba803968c0ad7a5079ac
rafacalera <rafa_calera@hotmail.com>
2023-11-05 23:09:20 -0300

    refactor curriculum data retrieval

--- helpers/getEffect.js
+++ helpers/getEffect.js
@@ -21,20 +21,62 @@ export default function getEffect(
         },
       })
       .then((response) => {
-        const { id, ...basicInfos } = response.data;
+        const {
+          basicInfos,
+          academicEducation,
+          commonKnowledge,
+          extraCourses,
+          language,
+          professionalExperience,
+        } = response.data;
         const basicInfosWithEmptyStrings = Object.fromEntries(
           Object.entries(basicInfos).map(([key, value]) => [key, value ?? ""]),
         );
-        dispatch(addCurriculum({ basicInfos: basicInfosWithEmptyStrings }));
+
+        academicEducation.items.forEach((item) => {
+          const dataInicioSplit = item.dataInicio.split("-");
+          const dataFimSplit = item.dataFim?.split("-");
+          item.dataInicio = dataInicioSplit[0] + "-" + dataInicioSplit[1];
+          item.dataFim = item.dataFim
+            ? dataFimSplit[0] + "-" + dataFimSplit[1]
+            : "";
+        });
+        extraCourses.items.forEach((item) => {
+          const dataInicioSplit = item.dataInicio.split("-");
+          const dataFimSplit = item.dataFim?.split("-");
+          item.dataInicio = dataInicioSplit[0] + "-" + dataInicioSplit[1];
+          item.dataFim = item.dataFim
+            ? dataFimSplit[0] + "-" + dataFimSplit[1]
+            : "";
+        });
+        professionalExperience.items.forEach((item) => {
+          const dataInicioSplit = item.dataInicio.split("-");
+          const dataFimSplit = item.dataFim?.split("-");
+          item.dataInicio = dataInicioSplit[0] + "-" + dataInicioSplit[1];
+          item.dataFim = item.dataFim
+            ? dataFimSplit[0] + "-" + dataFimSplit[1]
+            : "";
+        });
+        dispatch(
+          addCurriculum({
+            basicInfos: basicInfosWithEmptyStrings,
+            academicEducation: academicEducation,
+            commonKnowledge: commonKnowledge,
+            extraCourses: extraCourses,
+            language: language,
+            professionalExperience: professionalExperience,
+          }),
+        );
         setLoading(false);
       })
       .catch((err) => {
-        if (err.response.data === "Token expired") {
+        if (err.response?.data === "Token expired") {
           alert("Sua sessão expirou, faça login novamente");
           dispatch(logoutUser());
           router.push("/login");
           return;
         }
+        console.log(err);
       });
   }
   if (currentCurriculum) setLoading(false);
--- pages/api/user/info.js
+++ pages/api/user/info.js
@@ -1,17 +1,69 @@
 const database = require("../../../infra/db");
 const User = require("../../../models/user");
+const AcademicEducation = require("../../../models/academicEducation");
+const CommonKnowledge = require("../../../models/commonKnowledge");
+const ExtraCourses = require("../../../models/extraCourses");
+const Languages = require("../../../models/languages");
+const ProfessionalExpirience = require("../../../models/professionalExpirience");
 import verifyJWT from "../middleware";
 
 export async function getInfos(req, res) {
   const userId = req.id;
   if (req.method === "GET") {
     await database.sync();
-    const usuario = await User.findByPk(userId, {
-      attributes: { exclude: ["senha", "createdAt", "updatedAt"] },
-    });
 
+    const usuario = await User.findByPk(userId, {
+      attributes: { exclude: ["id", "senha", "createdAt", "updatedAt"] },
+    });
+    const usuarioAcademicEducation = await AcademicEducation.findAll({
+      attributes: { exclude: ["alunoId", "id", "createdAt", "updatedAt"] },
+      where: {
+        alunoId: userId,
+      },
+    });
+    const usuarioCommonKnowledge = await CommonKnowledge.findAll({
+      attributes: { exclude: ["alunoId", "id", "createdAt", "updatedAt"] },
+      where: {
+        alunoId: userId,
+      },
+    });
+    const usuarioExtraCourses = await ExtraCourses.findAll({
+      attributes: { exclude: ["alunoId", "id", "createdAt", "updatedAt"] },
+      where: {
+        alunoId: userId,
+      },
+    });
+    const usuarioLanguages = await Languages.findAll({
+      attributes: { exclude: ["alunoId", "id", "createdAt", "updatedAt"] },
+      where: {
+        alunoId: userId,
+      },
+    });
+    const usuarioProfessionalExpirience = await ProfessionalExpirience.findAll({
+      attributes: { exclude: ["alunoId", "id", "createdAt", "updatedAt"] },
+      where: {
+        alunoId: userId,
+      },
+    });
     usuario
-      ? res.status(200).json(usuario)
+      ? res.status(200).json({
+          basicInfos: usuario,
+          academicEducation: {
+            items: usuarioAcademicEducation,
+          },
+          commonKnowledge: {
+            items: usuarioCommonKnowledge,
+          },
+          extraCourses: {
+            items: usuarioExtraCourses,
+          },
+          language: {
+            items: usuarioLanguages,
+          },
+          professionalExperience: {
+            items: usuarioProfessionalExpirience,
+          },
+        })
       : res.status(400).send("User not found");
     return;
   }
